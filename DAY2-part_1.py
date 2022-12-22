134a690bf71964fc6fda06d3274353dd39cf8e13
Arsenii Gabov <102750985+Agabov123@users.noreply.github.com>
2022-12-22 14:27:59 +0000

    Update DAY2-part_1.py

--- DAY2-part_1.py
+++ DAY2-part_1.py
@@ -1,9 +1,33 @@
-def get_highest(f):
-    with open(f, 'r') as file:
-        list = []
-        for i in file.read().split('\n'):
-            list.append(i)
-        
-    
+with open('day2part1.txt', 'r') as file:
+    list = []
+    for i in file.read().split('\n'):
+        list.append(i)
+
+def get_points():
+    value1 = 0
+    value2 = 0
+    score1 = 0
+    score2 = 0
+    count = 0
+    for x in list:
+        if x == 'A X':
+            score2 += 4
+        elif x == 'B X':
+            score2 += 1
+        elif x == 'C X':
+            score2 += 7
+        elif x == 'A Y':
+            score2 += 8
+        elif x == 'B Y':
+            score2 += 5
+        elif x == 'C Y':
+            score2 += 2
+        elif x == 'A Z':
+            score2 += 3
+        elif x == 'B Z':
+            score2 += 9
+        elif x == 'C Z':
+            score2 += 6 
     print(list)
-get_highest('day2part1.txt')
+    print(score2)
+get_points()

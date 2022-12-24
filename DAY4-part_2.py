4fcb88c215fbf1d7486e6712e3ba61f6d9290376
Arsenii Gabov <102750985+Agabov123@users.noreply.github.com>
2022-12-24 05:15:14 +0000

    Solved DAY4-part_2.py

---
+++ DAY4-part_2.py
@@ -0,0 +1,21 @@
+with open('day4part1.txt', 'r') as file:
+    list = []
+    for i in file.read().split('\n'):
+        list.append(i)
+def get_number ():
+    count = 0
+    for i in list:
+        tasks = i.split(',')
+        task1 = tasks[0].split('-')
+        task2 = tasks[1].split('-')
+        sections1 = []
+        sections2 = []
+        for j in range(int(task1[0]), int(task1[1])+1):
+            sections1.append(j)
+        for y in range(int(task2[0]), int(task2[1])+1):
+            sections2.append(y)
+        if any(item in sections1 for item in sections2) or any(item in sections2 for item in sections1):
+            count += 1
+ 
+    print(count)
+get_number()

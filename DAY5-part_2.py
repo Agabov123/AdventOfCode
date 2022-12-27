ddf9967d8c268323e481623b25734078455df237
Arsenii Gabov <102750985+Agabov123@users.noreply.github.com>
2022-12-27 07:51:50 +0000

    solution part 2

---
+++ DAY5-part_2.py
@@ -0,0 +1,30 @@
+with open('day5part1.txt', 'r') as file:
+    list = []
+    for i in file.readlines():
+        action = []
+        for x in i.strip().split(' '):
+            action.append(x)
+        list.append(action)
+
+containers = [['Q','S','W','C','Z','V','F','T'], ['Q','R','B'], ['B','Z','T','Q','P','M','S'], ['D','V','F','R','Q','H'], ['J','G','L','D','B','S','T','P'], ['W','R','T','Z'], ['H','Q','M','N','S','F','R','J'], ['R','N','F','H','W'], ['J','Z','T','Q','P','R','B']]
+
+def solution():
+    for i in list:
+        print(i)
+        movefrom = int(i[3])-1
+        moveto = int(i[5])-1
+        letters = []
+        for x in range(int(i[1])):
+            l = containers[movefrom][-1]
+            del(containers[movefrom][-1])
+            letters.append(l)
+        letters.reverse()
+        for letter in letters:
+            containers[moveto].append(letter)
+        print(l)
+        
+    solution = ''
+    for container in containers:
+        solution += container[-1]
+    print(solution)
+solution()

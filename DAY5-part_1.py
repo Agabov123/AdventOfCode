91567879dce1d5532f41caf33d1696a40d7825f9
Arsenii Gabov <102750985+Agabov123@users.noreply.github.com>
2022-12-27 07:42:20 +0000

    part1 solved

--- DAY5-part_1.py
+++ DAY5-part_1.py
@@ -1,10 +1,26 @@
-with open('day5part1.txt', 'r') as f:
-    list = f.readlines()
-    list = [entry for entry in list]
-def move():
-    number_of_crates = len(list[0])//4
-    crate_lines = list[:list.index('\n')-1]
-    moving_lines = list[list.index('\n')+1:]
-    print(number_of_crates, crate_lines, moving_lines)
-    
-move()
+with open('day5part1.txt', 'r') as file:
+    list = []
+    for i in file.readlines():
+        action = []
+        for x in i.strip().split(' '):
+            action.append(x)
+        list.append(action)
+
+containers = [['Q','S','W','C','Z','V','F','T'], ['Q','R','B'], ['B','Z','T','Q','P','M','S'], ['D','V','F','R','Q','H'], ['J','G','L','D','B','S','T','P'], ['W','R','T','Z'], ['H','Q','M','N','S','F','R','J'], ['R','N','F','H','W'], ['J','Z','T','Q','P','R','B']]
+
+def solution():
+    for i in list:
+        print(i)
+        movefrom = int(i[3])-1
+        moveto = int(i[5])-1
+        for x in range(int(i[1])):
+            l = containers[movefrom][-1]
+            del(containers[movefrom][-1])
+            containers[moveto].append(l)
+            print(l)
+        
+    solution = ''
+    for container in containers:
+        solution += container[-1]
+    print(solution)
+solution()
